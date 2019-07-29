6d0a2348a81d8011c5c665d88ff7fd4dc4b9edc0
suluo <sam_suluo@163.com>
2019-07-29 22:52:24 +0800

    feat(Loss): Loss

---
+++ Loss.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+############################################
+# File Name    : Loss.py
+# Created By   : Suluo - sampson.suluo@gmail.com
+# Creation Date: 2019-07-29
+# Last Modified: 2019-07-29 22:51:56
+# Descption    :
+# Version      : Python 3.7
+############################################
+import argparse
+import time
+import os
+import torch
+from torchcrf import CRF
+
+
+class Loss(object):
+    def __init__(self, opt):
+        self.padding_idx = opt.padding_idx
+
+    def cal_performance(self, pred, gold, smoothing=False):
+        loss = self.cal_loss(pred, gold, smoothing)
+        n_correct = self.cal_correct(pred, gold)
+
+    def cal_loss(self, pred, gold, smoothing=False):
+        raise NotImplementedError()
+
+    def cal_correct(self, pred, gold):
+        pred = torch.argmax(pred, dim=-1)
+        non_pak_mask = gold.ne(self.padding_idx)
+        n_correct = pred.eq(gold)
+        n_correct = n_correct.masked_select(non_pak_mask).sum().item()
+        return n_correct
+
+
+class CRFLoss(Loss):
+    def __init__(self, opt, num_tags):
+        super().__init__(opt)
+        self.crf = CRF(num_tags, batch_first=True).to(opt.device)
+
+    def cal_loss(self, pred, gold, smotthing):
+        loss = -self.crf(pred, gold, mask=self.padding_idx)
+        return loss
+
+
+class CrossEntropy(Loss):
+    def __init__(self, opt):
+        super().__init__(opt)
+
+    def cal_loss(self, pred, gold, smoothing):
+        ''' Calculate cross entropy loss, apply label smoothing if needed. '''
+
+        gold = gold.contiguous().view(-1)
+
+        if smoothing:
+            eps = 0.1
+            n_class = pred.size(1)
+
+            one_hot = torch.zeros_like(pred).scatter(1, gold.view(-1, 1), 1)
+            one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
+            log_prb = F.log_softmax(pred, dim=1)
+
+            non_pad_mask = gold.ne(self.padding_idx)
+            loss = -(one_hot * log_prb).sum(dim=1)
+            loss = loss.masked_select(non_pad_mask).sum()  # average later
+        else:
+            loss = F.cross_entropy(pred, gold, ignore_index=self.padding_idx, reduction='sum')
+
+        return loss
+
+
+def main(args):
+    return args
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--num', type=int, default=100, help='input num')
+    args = parser.parse_args()
+    main(args)
---
+++ nn/AttnLayer.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+############################################
+# File Name    : AttnLayer.py
+# Created By   : Suluo - sampson.suluo@gmail.com
+# Creation Date: 2019-03-07
+# Last Modified: 2019-07-29 22:39:37
+# Descption    :
+# Version      : Python 3.7
+############################################
+import argparse
+import time
+import os
+import sys
+import torch
+import torch.nn.functional as F
+
+
+class GlobalAttention(torch.nn.Module):
+    def __init__(self, method, hidden_size):
+        super().__init__()
+        self.method = method
+        self.hidden_size = hidden_size
+
+        if self.method not in ['dot', 'general', 'concat']:
+            raise ValueError(self.method, "is not an appropriate attention method.")
+        if self.method == 'general':
+            self.attn = torch.nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = torch.nn.Linear(self.hidden_size * 2, hidden_size)
+            self.other = torch.nn.Parameter(torch.FloatTensor(1, hidden_size))
+
+    def forward(self, ht, enc_outputs):
+        """
+        Args:
+            ht: (1, batch, 512)
+            enc_outputs: (batch, seq_len, 512): 所有的hs
+        Return:
+            context: (batch, 512)
+            attn_weights: (batch, 1, seq_len)
+            # 输出是注意力的概率，也就是长度为input_lengths的向量，它的和加起来是1。
+        """
+        attn_energies = self.score(ht.tranpose(0, 1), encoder_outputs)
+
+        # (batch, seq_len) -> (batch, 1, seq_len)
+        attn_weights = F.softmax(attn_energies, dim=1).unsqueeze(1)
+        context = attn_weights.bmm(enc_outputs)
+        return context, attn_weights
+
+    def score(self, ht, enc_outputs):
+        if self.method == 'dot':
+            energy = torch.sum(ht * enc_outputs, dim=2)
+        elif self.method == 'general':
+            energy = self.attn(enc_outputs)
+            energy = torch.sum(ht * energy, dim=2)
+        elif self.method == 'concat':
+            energy = self.attn(torch.cat((ht.expand(enc_outputs.size(0), -1, -1), enc_outputs), 2)).tanh()
+            energy = torch.sum(self.other * energy, dim=2)
+        return energy
+
+
+def main(args):
+    return args.num
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--num', type=int, default=100, help='input num')
+    args = parser.parse_args()
+    main(args)
