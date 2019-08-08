ed69b913ade58177be847811db1e14a284b47f30
orion <orion@oriondeMacBook-Pro.local>
2019-08-08 13:06:59 +0800

    feat(encoder): rnn

--- nn/Encoder/rnn.py
+++ nn/Encoder/rnn.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 ############################################
-# File Name    : Encoder.py
+# File Name    : rnn.py
 # Created By   : Suluo - sampson.suluo@gmail.com
 # Creation Date: 2019-07-29
-# Last Modified: 2019-07-30 10:56:44
+# Last Modified: 2019-08-08 13:05:44
 # Descption    :
 # Version      : Python 3.7
 ############################################
@@ -14,38 +14,55 @@ import os
 import sys
 import Constants
 
-from .RNN import RNN
 from torch import nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
-class EncoderRNN(RNN):
+
+class EncoderRNN(nn.Module):
     r"""
     Applies a multi-layer RNN to an input sequence.
+    Kwargs:
+        num_embeddings
+        word2vec_size
+        rnn_type
+        rnn_size
+        layers
+        dropout
+        brnn
 
+        pretrained_embed
     Args:
 		Inputs: inputs, input_lengths
 		Outputs: output, hidden
     Examples::
-
          >>> encoder = EncoderRNN(input_vocab, max_seq_length, hidden_size)
          >>> output, hidden = encoder(input)
-
     """
 
-    def __init__(self, num_embeddings, word2vec_size, rnn_type, rnn_size,
-                 layers, dropout, brnn, embeddings=None):
-        embedding_dim = word2vec_size
+    def __init__(self, **kwargs):
+        super().__init__()
 
-        super().__init__(rnn_type,
-                         word2vec_size,
-                         rnn_size,
-                         layers,
-                         True,
-                         dropout,
-                         brnn)
-        self.hidden_size = rnn_size
-        self.embedding = nn.Embedding(num_embeddings, embedding_dim, Constants.PAD)
+        for k in kwargs:
+            self.__setattr__(k, kwargs[k])
+
+        V = self.num_embeddings
+        D = self.word2vec_size
+
+        self.hidden_size = self.rnn_size
+        self.embedding = nn.Embedding(V, D, Constants.PAD)
+        if self.pretrained_embed:
+            self.embedding.weight.data.copy_(self.pretrained_weight)
+
+        self.rnn = getattr(nn, self.rnn_type)(
+            embedding_dim=D,
+            hidden_size=self.rnn_size,
+            num_layers=self.layers,
+            batch_first=True,
+            dropout=self.dropout,
+            bidirectional=self.brnn
+        )
+
 
     def forward(self, input_var, input_lengths=None):
         """
