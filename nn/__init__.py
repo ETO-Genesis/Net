695d270cf3a51fa1a6bfcc7d84bc430be2a342f8
suluo <sampson.suluo@gmail.com>
2019-08-16 16:59:53 +0800

    Feat(rnn): rnn

--- nn/__init__.py
+++ nn/__init__.py
@@ -4,8 +4,10 @@
 # File Name    : __init__.py
 # Created By   : Suluo - sampson.suluo@gmail.com
 # Creation Date: 2019-08-13
-# Last Modified: 2019-08-16 15:59:18
+# Last Modified: 2019-08-16 16:59:35
 # Descption    :
 # Version      : Python 3.7
 ############################################
 from .modules.sparse import Embedding
+
+from .modules.rnn import RNN
---
+++ nn/modules/rnn.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+############################################
+# File Name    : rnn.py
+# Created By   : Suluo - sampson.suluo@gmail.com
+# Creation Date: 2019-08-16
+# Last Modified: 2019-08-16 16:59:01
+# Descption    :
+# Version      : Python 3.7
+############################################
+import argparse
+from torch import nn
+
+
+class RNN(nn.Module):
+    def __init__(self, rnn_type, input_size, hidden_size, num_layers,
+                 batch_first=True, dropout=0.1, bidirectional=True):
+
+        self.rnn = getattr(nn, self.rnn_type)(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout,
+            bidirectional=bidirectional
+        )
+
+    def forward(self, embed, hidden=None):
+        output, hidden = self.rnn(embed, hidden)
+        return output, hidden
+
+
+def main(args):
+    return args
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--num', type=int, default=100, help='inputnum')
+    args = parser.parse_args()
+    main(args)
